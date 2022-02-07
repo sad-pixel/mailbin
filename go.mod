35b1a2b16988a84b91c90a2a3cecaede9ed70959
Ishan Das Sharma <ishan.dassharma1@gmail.com>
2022-02-07 19:14:09 +0530

    add basic config from env

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/emersion/go-smtp v0.15.0
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/gust1n/go-render v0.0.0-20140901072858-0ff758e26fe7
+	github.com/joho/godotenv v1.4.0
 )
 
 require github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21 // indirect
--- main.go
+++ main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"log"
+	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/sad-pixel/mailbin/email"
 	"github.com/sad-pixel/mailbin/repository"
 	"github.com/sad-pixel/mailbin/web"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("could not read config files, will use default configuration instead")
+		os.Setenv("MAILBIN_SMTP_PORT", "1025")
+		os.Setenv("MAILBIN_HTTP_PORT", "1026")
+	}
+
 	emailRepository := &repository.EmailRepository{}
 	emailSettings := &email.Settings{
-		Port:              ":1025",
+		Port:              ":" + os.Getenv("MAILBIN_SMTP_PORT"),
 		Host:              "localhost",
 		ReadTimeout:       10 * time.Second,
 		WriteTimeout:      10 * time.Second,
@@ -23,7 +31,7 @@ func main() {
 	go email.ListenAndServe(emailSettings, emailRepository)
 
 	webSettings := &web.WebSettings{
-		Port: ":1026",
+		Port: ":" + os.Getenv("MAILBIN_HTTP_PORT"),
 	}
 
 	webHandlers, err := web.NewHandlers(emailRepository)
